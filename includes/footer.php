19bea75583ef931b165348c1cfd40300f94069cf
full4metal <58386546+full4metal@users.noreply.github.com>
2024-03-28 21:27:18 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b></b>
+      </div>
+      <strong><a href="https://github.com/harikutty5896/online-voting-system-using-PHP" target="_blank"></a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
